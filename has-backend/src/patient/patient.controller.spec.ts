8f1940e491568084703f9ecac04cff6eab30225a
Mirla Chucre <mirlabraga@gmail.com>
2019-08-26 18:16:01 +1000

    Fix tests of the Patient

--- has-backend/src/patient/patient.controller.spec.ts
+++ has-backend/src/patient/patient.controller.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientController } from './patient.controller';
+import { PatientsSchema } from '../schemas/patients.schemas';
+import { getModelToken } from '@nestjs/mongoose';
+import { PatientService } from './patient.service';
 
 describe('Patient Controller', () => {
   let controller: PatientController;
 
+  const mockRepository = {
+    find() {
+      return {};
+    }
+  };
+
+  const mockMongooseTokens = [
+    {
+      provide: getModelToken('Patients'),
+      useValue: {},
+    },
+  ];
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientController],
-    }).compile();
+      providers: [PatientService, ...mockMongooseTokens],
+    }).overrideProvider(getModelToken('Patients'))
+    .useValue(mockRepository).compile();
 
     controller = module.get<PatientController>(PatientController);
   });
--- has-backend/src/patient/patient.service.spec.ts
+++ has-backend/src/patient/patient.service.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientService } from './patient.service';
+import { AppModule } from '../app.module';
+import { getModelToken } from '@nestjs/mongoose';
+
 
 describe('PatientService', () => {
   let service: PatientService;
 
+  const mockRepository = {
+    find() {
+      return {};
+    }
+  };
+
+  const mockMongooseTokens = [
+    {
+      provide: getModelToken('Patients'),
+      useValue: {},
+    },
+  ];
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientService],
-    }).compile();
+      imports: [AppModule],
+      providers: [PatientService, ...mockMongooseTokens],
+    }).overrideProvider(getModelToken('Patients'))
+    .useValue(mockRepository).compile();
 
     service = module.get<PatientService>(PatientService);
   });
